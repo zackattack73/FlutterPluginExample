6c273a14edd60afbadb14243f8f15bf4669e7fe0
Shi Hao Hong <shihaohong94@gmail.com>
2020-11-24 10:32:30 +0800

    Get app working

---
+++ lib/l10n/translations_en.dart
@@ -0,0 +1,14 @@
+
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'translations.dart';
+
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+/// The translations for English (`en`).
+class TranslationsEn extends Translations {
+  TranslationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get example_title => 'Title in English';
+}
---
+++ lib/l10n/translations_fr.dart
@@ -0,0 +1,14 @@
+
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'translations.dart';
+
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+/// The translations for French (`fr`).
+class TranslationsFr extends Translations {
+  TranslationsFr([String locale = 'fr']) : super(locale);
+
+  @override
+  String get example_title => 'Titre en Français';
+}
